808e6a726b07c8a4bab0557ed21e4d45d3e5c721
Douglas <dgarrigan@me.com>
2020-09-27 02:48:49 +0000

    _

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -1,14 +1,16 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
+import { Box } from 'rebass';
 
 
 
-// pure componet 
+
+
 function reducer(state, action) {
 	switch(action.type) {
-		case 'increment':
-			return state + 1;
-		case 'decrement': 
-			return state - 1;
+		case "add-todo":
+			return {
+				todos: [...state.todos, {text: action.payload, completed: false }] 
+			};
 		default:
 			return state;
 	}
@@ -19,16 +21,68 @@ function reducer(state, action) {
 
 const Reducer = () => {
 	
-	const [count, dispatch] = useReducer(reducer, 0)
 	
+	
+	const [{ todos }, dispatch] = useReducer(reducer, { todos: [] });
+	const [text, setText] = useState();
+
 	return(
 		<>
-			<div>count: {count}</div>
-			<button onClick={() => dispatch({ type: 'increment', })}>Increment</button>
-			<button onClick={() => dispatch({ type: 'decrement', })}>Decrement</button>
+		
+			<Box width={1}>
+				<form onSubmit={e => {
+					e.preventDefault();
+					dispatch({ type: "add-todo", payload: text })
+					setText("");
+				}}>
+					<input value={text} onChange={e => setText(e.target.value)} />
+				</form>
+			</Box>
+			
+			
+			{todos.map(t => (
+				<Box width={1} key={t.text}>{t.text}</Box>
+			))}
+			
+			
 		</>
 	)
+	
+	
 }
+
+
+
+
+
+
+
+// pure componet 
+// function reducer(state, action) {
+// 	switch(action.type) {
+// 		case 'increment':
+// 			// avoid mutating state with ++
+// 			return state + 1;
+// 		case 'decrement': 
+// 			return state - 1;
+// 		default:
+// 			return state;
+// 	}
+// }
+
+
+
+
+// const Reducer = () => {
+// 	const [count, dispatch] = useReducer(reducer, 0)
+// 	return(
+// 		<>
+// 			<div>count: {count}</div>
+// 			<button onClick={() => dispatch({ type: 'increment', })}>Increment</button>
+// 			<button onClick={() => dispatch({ type: 'decrement', })}>Decrement</button>
+// 		</>
+// 	)
+// }
 
 
 
